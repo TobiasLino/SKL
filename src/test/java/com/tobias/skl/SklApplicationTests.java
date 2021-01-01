84f2b7e825dc00a9a7966d7309504cb895308cfb
TobiasLino <tobias.lino@sonda.com>
2021-01-01 20:21:26 -0300

    adição da base de arquivos do backend

---
+++ src/test/java/com/tobias/skl/SklApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.skl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SklApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
